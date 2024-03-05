38a1f30f4a140dd4269986682c1bc0987f8e526e
Nhan <nhanptde160251@fpt.edu.vn>
2024-03-05 13:28:09 +0900

    WebDB Context half finished

---
+++ Pre-Mock/Database/WebDBContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Pre_Mock.Models.Orders;
+using Pre_Mock.Models.Products;
+using Pre_Mock.Models.Users;
+
+namespace Pre_Mock.Database
+{
+    public class WebDBContext : DbContext
+    {
+        public WebDBContext() { }
+        public WebDBContext(DbContextOptions<WebDBContext> options) : base(options) { }
+        DbSet<Order> orders { get; set; }
+        DbSet<OrderDetails> orderDetails { get; set; }
+        DbSet<Category> categories { get; set; }
+        DbSet<Program> programs { get; set; }
+        DbSet<BuyUser> buyUsers { get; set; }
+        DbSet<Role> roles { get; set; }
+        DbSet<User> users { get; set; }
+        DbSet<UserDetails> userDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
